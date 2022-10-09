ab36bda1febc1d027d59c3937c285eb5fde1712f
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-09 19:18:34 +0200

    P5 PHP

--- EX_01/index.php
+++ EX_01/index.php
@@ -3,3 +3,4 @@
 //EX_01 Utiliza las cookies para contar el número de visitas que realiza el usuario. Este
 //contador tardará una hora en volver a contar.
 
+setcookie('contador','40px',time()+3600)
