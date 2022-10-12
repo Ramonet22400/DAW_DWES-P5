573a9637b9b347049942a70cb3700941ae430387
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-12 20:10:49 +0200

    P5 PHP

---
+++ EX_02/cookies.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_GET['Enviar'])){
+    $valor_nombre = $_GET['Nombre'];
+    $valor_idioma  = $_GET['Idioma'];
+}
+
+setcookie('Nombre', $valor_nombre);
+setcookie('Idioma',$valor_idioma);
+
+?>
+
+<form action="cookies.php" method="get">
+    <input type="text" name="Nombre" placeholder="Escriba su nombre" required>
+    <input list ="browser" name="Idioma" id="Idioma" placeholder="¿Que idioma prefiere?" required>
+
+        <datalist id="browser">
+            <option value="Castellano">
+            <option value="Catalan">
+            <option value="English">
+            </option>
+        </datalist>
+   <input type="submit" name="Enviar" value="Enviar preferencias">
+
+</form>
--- EX_02/index.php
+++ EX_02/index.php
@@ -1,1 +1,13 @@
-<form action
+<form action="cookies.php" method="get">
+    <input type="text" name="Nombre" placeholder="Escriba su nombre" required>
+    <input list ="browser" name="Idioma" id="Idioma" placeholder="¿Que idioma prefiere?" required>
+
+        <datalist id="browser">
+            <option value="Castellano">
+            <option value="Catalan">
+            <option value="English">
+            </option>
+        </datalist>
+   <input type="submit" name="Enviar" value="Enviar preferencias">
+
+</form>
