9405ecc605735c5d91e7abe897f37460a066a121
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-19 12:19:54 +0200

    P5 PHP

--- EX_04/index.php
+++ EX_04/index.php
@@ -5,3 +5,20 @@
 //permisos (si son o no administradores) y una imagen (que, por el momento, no tendrá
 //ningún valor).
 
+
+$conexion = new PDO('mysql:host=fmesasc.com;dbname=daw2', 'daw2', 'Gimbernat');
+
+$resultados = $conexion->query("CREATE TABLE RamonPerezM_tablas(
+                                id INT(6),
+                                firstname VARCHAR(30),
+                                lastname VARCHAR(40),
+                                email VARCHAR(30),
+                                contra VARCHAR(40),
+                                fecha_nacimiento DATE,
+                                permisos BOOLEAN,
+                                imagen VARCHAR(2)
+                                )");
+
+print $conexion;
+
+
---
+++ EX_05/index.php
@@ -0,0 +1,7 @@
+<?php
+
+//5. Cree una página PHP que permita realizar el inicio de sesión y registrar a un usuario
+//en la base de datos con sus diferentes valores. Por defecto, no tienen que ser
+//administradores, este permiso por el momento siempre será false.
+
+
